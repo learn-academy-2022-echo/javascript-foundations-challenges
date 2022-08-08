f90f8ff76d9db1110f8a89ff5f9a6ffaab5bebef
TuanLeVietz <tuanz_z@yahoo.com>
2022-08-08 16:51:42 -0700

    all answers except last

--- higher-order-bw-hp-jm-tl.js
+++ higher-order-bw-hp-jm-tl.js
@@ -32,15 +32,40 @@ const oddNumber = (array) => {
 console.log(oddNumber(arr2))
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use typeof method
-// var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"];
+var comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"];
 // // --> "nicework"
+//PSEUDOCODE: 
+//declare a function named sentence
+//utilize .filter method to remove anyting that is not a letter
+//use .join method to combine all letters together to create one string
+//expected output: "nicework"
+
+const sentence = comboArr.filter((value) => {
+        return typeof value === "string"
+    })
+
+console.log(sentence.join(""))
+
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
-// var filterArrayValues = [58, " ", "abcd", true, null, false, 0];
+var filterArrayValues = [58, " ", "abcd", true, null, false, 0];
+//PSEUDOCODE
+//declare a function missVal 
+//filter function to remove values of false, null, 0, blank.
+//expected outcome [58, "abcd", true]
+
+const missVal = filterArrayValues.filter((value) => {
+    if (value !== " " || value !== 0 || value !== false || value !== null){
+        return value
+    } 
+
+})
+console.log(missVal.splice(1, 1))
+
 // // --> [58, "abcd", true]
 // STRETCH Challenges
 
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-// var str = "javascript is awesome";
+//var str = "javascript is awesome";
 // // --> "jvscrpt s wsm"
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 // var arr1 = [3, 7, 10, 5, 4, 3];
