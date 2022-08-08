8d04cbb5a872c80cc36ee0e797aee89af78ddc52
dnlclmnt <dnlclmnt@gmail.com>
2022-08-08 13:03:09 -0700

    finished second set of questions

--- fla-tuan-daniel.js
+++ fla-tuan-daniel.js
@@ -73,13 +73,14 @@ console.log(lettersOnly(comboArr))
 // // --> "nicework"
 
 // Create a function that takes in an array of numbers and returns the sum.
+var addThese1 = [1, 2, 3, 4];
+
 //create a function that takes in an array
 //create sum variable set to 0
     //iterate over passed in array
     //add sum with array[i]
     //return sum
 
-var addThese1 = [1, 2, 3, 4];
 const addedUp = (arr) => {
     let sum = 0
     for (let i = 0; i < arr.length;i++){
@@ -95,30 +96,100 @@ var addThese2 = [];
 console.log(addedUp(addThese2))
 // // --> 0
 // Create a function that takes in an array of numbers and returns the index of the largest number.
-// var indexHighestNumber = [1, 4, 2, 3];
+var indexHighestNumber = [1, 4, 2, 3];
+// Create function that takes in an array
+// create largestNum variable set to first item in array
+// iterate over array
+    // if array[i] > largestNum
+            // largestNum = array[i]
+// return indexOf largestNum
+
+const largestNumFunc = (arr) => {
+    let largestNum = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largestNum) {
+            largestNum = arr[i];
+        }
+    }
+    let indexOfLargest = arr.indexOf(largestNum)
+    return indexOfLargest;
+}
+
+console.log(largestNumFunc(indexHighestNumber))
+
+
+
 // // --> 1
 // STRETCH Challenges
 
 // Create a function that takes in two arrays and returns one array with no duplicate values.
-// var arr1 = [3, 7, 10, 5, 4, 3, 3];
-// var arr2 = [7, 8, 2, 3, 1, 5, 4];
+var arr1 = [3, 7, 10, 5, 4, 3, 3];
+var arr2 = [7, 8, 2, 3, 1, 5, 4];
+
+// create function that takes in two arrays
+    // create bothArr array by concatenating both arrays
+    // iterate over bothArr 
+
+
+
+    const noDupes = (arrayOne, arrayTwo) => {
+        let bothArr = arrayOne.concat(arrayTwo);
+        let result = [];
+        for (let i = 0; i < bothArr.length; i++) {
+            if (result.indexOf(bothArr[i]) === -1) {
+                result.push(bothArr[i])
+            }
+        }
+        return result;
+    }
+
+    console.log(noDupes(arr1, arr2))
+
+
 // // --> [3, 7, 10, 5, 4, 8, 2, 1]
 // Create a function that takes in two numbers as arguments and returns an array the length of the first number filled with the second number.
-// var arrayLength = 6;
-// var arrayValue = 0;
+var arrayLength = 6;
+var arrayValue = 0;
 // // --> [0, 0, 0, 0, 0, 0]
 
-// var arrayLength = 4;
-// var arrayValue = 11;
+
+const firstNumFunc = (num1, num2) => {
+    let result = [];
+
+    for (let i =0; i < num1; i++) {
+        result.push(num2)
+    }
+    return result;
+}
+
+console.log(firstNumFunc(arrayLength, arrayValue))
+
+
+var arrayLength = 4;
+var arrayValue = 11;
+console.log(firstNumFunc(arrayLength, arrayValue))
+
 // // --> [11, 11, 11, 11]
 // Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function.
-// var addUp1 = 4;
+var addUp1 = 4;
 // // 1 + 2 + 3 + 4 = 10
 // // --> 10
 
-// var addUp2 = 10;
+var addUp2 = 10;
 // // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = 55
 // // --> 55
 
-// var addUp3 = 600;
+var addUp3 = 600;
 // // --> 180300
+
+const addFunc = (num) => {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(addFunc(addUp1));
+console.log(addFunc(addUp2));
+console.log(addFunc(addUp3));
