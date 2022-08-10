2ffa596618f4451529e4c11ca2a639debab47480
ElijahBcodes <eli1inthesnow@gmail.com>
2022-08-10 13:29:38 -0700

    objects-challenge

---
+++ objects-elijah-kelly.js
@@ -0,0 +1,152 @@
+// Challenges
+// Consider this variable:
+
+// const person = {
+//   firstName: "Arthur",
+//   lastName: "Dent",
+// };
+// Write the code that accesses the first name of the person object.
+
+// console.log(person.firstName)
+// // Write the code that accesses the last name of the person object.
+// console.log(person.lastName)
+// // Write the code that gives the person object a property of homePlanet and set it to 'Earth'.
+// person.homePlanet = 'Earth'        
+
+// console.log(person)
+// Update the person object with a method that logs "Arthur Dent is from planet Earth".
+// Consider this variable:
+// person.sentence = () => {
+//     return 
+// }
+
+// const person = {
+//     firstName: "Arthur",
+//     lastName: "Dent",
+//     homePlanet: "Earth",
+//     sentence: function () {
+//         return `${this.firstName} ${this.lastName} is from planet ${this.homePlanet}`
+//     }
+
+//   };
+// console.log(person.sentence())
+
+// const product = {
+//   name: "chair",
+//   price: 24.99,
+// };
+// Write a function called describeProduct that takes the product object as an argument and logs "The product is a chair. It costs $24.99".
+//Pseudocode: create function called describeProduct 
+//input: a product object
+//output: logs "The product is a chair. It costs $24.99".
+//use string interpolation to return string
+//access object values using dot notation
+// const describeProduct = (obj) => {
+//     return `The product is a ${obj.name}. It costs $${obj.price}.`
+// }
+// console.log(describeProduct(product))
+
+// Write a function called totalWithTax that takes the product object as an argument and returns the total price of the chair that includes a 7% sales tax rounded to two decimals.
+// const product = {
+//     name: "chair",
+//     price: 24.99,
+//   };
+// //Pseudocode:write a function called totalWithTax
+// //input: product object
+// //output: price of chair with 7% sales tax 
+// //return Math.round(multiply by 100) divided by 100 will round the number two decimals
+// const totalWithTax = (obj) => {
+//     return Math.round(obj.price * 1.07*100)/100
+// }
+
+// console.log(totalWithTax(product));
+// Consider this variable:
+const lunch = {
+  name: "PB and Banana",
+  type: "sandwich",
+  ingredients: ["bread", "peanut butter", "banana"],
+};
+// Write the code that accesses the ingredients property.
+console.log(lunch.ingredients);
+// Write the code that access the third ingredient of the lunch object.
+console.log(lunch.ingredients[2]);
+// Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+//Pseudocode: write function named minions 
+//input: a lunch object
+//output:sentence that describes lunch object
+//use string interpolation and dot notation to return the output
+const minions = (food) => {
+    return `The ingridients for a ${food.name} ${food.type} are ${food.ingredients}.`
+}
+// console.log(minions(lunch));
+// Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+lunch.minions = minions
+console.log(lunch);
+
+
+
+// Consider this variable:
+
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 },
+];
+// Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+//Pseudocode: write a function named catsOnly 
+//input: an array of objects 
+//output: a new array with only objects that are cats 
+//use the filter method
+const catsOnly = (pets) => {
+    return pets.filter(value => value.type === "cat")
+}
+console.log(catsOnly(animals));
+
+// Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
+// Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
+// Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
+// Consider this variable:
+
+// const author = {
+//   name: "H. G. Wells",
+//   genre: "science fiction",
+// };
+// Write the code that destructures the author object so that the following code snippet will run successfully:
+// console.log(`${name} is a ${genre} author`);
+// // Output: "H. G. Wells is a science fiction author"
+// Consider this variable:
+
+// const pokeOne = {
+//   species: "Charmandar",
+//   pokemon_type: "Fire",
+// };
+
+// const pokeTwo = {
+//   species: "Magikarp",
+//   pokemon_type: "Water",
+// };
+// Create a function called describePokemon that take an object like the ones above and uses destructuring to return a description of the Pokemon so that the following code snippet will run successfully:
+// console.log(describePokemon(pokeOne));
+// // Output: "Charmandar is a Fire pokemon"
+// console.log(describePokemon(pokeTwo));
+// // Output: "Magikarp is a Water pokemon"
+// Consider this variable:
+
+// const triangleDimensions = {
+//   base: 2,
+//   height: 5,
+// };
+// Modify the triangleDimensions object to have a method that returns the area of the triangle.
+// Write the code that will update the base to be the value of 6.
+// Consider this variable:
+
+// const learn = {
+//   cohorts: {
+//     2021: ["Alpha", "Bravo", "Charlie", "Delta", "Echo"],
+//     2022: ["Alpha", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot"],
+//   },
+// };
+// Write the code that logs the name of your cohort.
+// Write the code that uses destructuring to log the name of your cohort.
+// Create a function that takes an object like the one above and returns an array with a string of every cohort name and year.
