831d225ff4b2a6f91db393873fbb0c6f3d77a090
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-08-10 10:59:52 -0700

    Final touches

--- jest-francisco-cody/jest.test.js
+++ jest-francisco-cody/jest.test.js
@@ -86,6 +86,7 @@ describe("smallOnly", () => {
 // if the first number is smaller than the second number
 // return first number
 // else return second number
+
 let numberOne = 10
 let numberTwo = 15
 
@@ -120,20 +121,26 @@ describe("oddNum", () => {
         }
     }
     
-
 // Write the test for a function that takes in a fruit and returns "yellow" if the argument is banana, "red" if apple and "purple" if grape.
 // Write the function that will make the test pass.
+
 // Write the test for a function called rick that returns "Morty".
 // Write the function that will make the test pass.
+
 // Write the test for a function called greeter that takes a name as an argument and returns a greeting with that name to the screen.
 // Write the function that will make the test pass.
+
 // Write the test for a function called oddOrEven that takes a number as an argument and logs whether the number is odd or even.
 // Write the function that will make the test pass.
+
 // Write the test for a function called doubler that takes a number and returns the result of the number multiplied by 2.
 // Write the function that will make the test pass.
+
 // Write the test for a function called multiply that takes two numbers as arguments and logs the result of one of the numbers multiplied by the other.
 // Write the function that will make the test pass.
+
 // Write the test for a function called divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 // Write the function that will make the test pass.
+
 // Write the test for a function called fizzbuzz. If a number is a multiple of 3, replace it with the word 'fizz'. If a number is a multiple of five, replace it with the word 'buzz'. If a number is a multiple of both 3 and 5, replace it with 'fizzbuzz'.
 // Write the function that will make the test pass.
